426698438793fc1da46a394643db41cafff0fec1
Ewout Quax <ewout.quax@bettyblocks.com>
2023-08-07 22:47:07 +0200

    Introduce board.validMoves
    All lines, that are not completely filled, are valid for the next move

--- internals/board/move.go
+++ internals/board/move.go
@@ -1,10 +1,23 @@
 package board
 
+import "sort"
+
 func (b *Board) MakeMove(lineIdx int, chip Chip) {
 	var rowIdx int
-	for rowIdx = 5; rowIdx > 0 && b.Line[lineIdx][rowIdx] == None; rowIdx-- {
+	for rowIdx = 5; rowIdx > 0 && b.Line[lineIdx][rowIdx-1] == None; rowIdx-- {
 		continue
 	}
 
 	b.Line[lineIdx][rowIdx] = chip
+}
+
+func (b *Board) ValidMoves() (valid []int) {
+	for idx, line := range b.Line {
+		if line[5] == None {
+			valid = append(valid, idx)
+		}
+	}
+	sort.Ints(valid)
+
+	return
 }
--- internals/board/move_test.go
+++ internals/board/move_test.go
@@ -12,3 +12,39 @@ func TestMove(t *testing.T) {
 
 	assert.Equal(t, "[0,0,0,0,1,0,0]", myBoard.toState())
 }
+
+func TestTwoMovesInTwoLines(t *testing.T) {
+	myBoard := Init()
+	myBoard.MakeMove(4, Red)
+	myBoard.MakeMove(3, Yellow)
+
+	assert.Equal(t, "[0,0,0,2,1,0,0]", myBoard.toState())
+}
+
+func TestTwoMovesInSameLine(t *testing.T) {
+	myBoard := Init()
+	myBoard.MakeMove(4, Red)
+	myBoard.MakeMove(4, Yellow)
+
+	assert.Equal(t, "[0,0,0,0,7,0,0]", myBoard.toState())
+}
+
+func TestValidMovesOnInitBoard(t *testing.T) {
+	myBoard := Init()
+	moves := myBoard.ValidMoves()
+
+	assert.Equal(t, []int{0, 1, 2, 3, 4, 5, 6}, moves)
+}
+
+func TestValidMovesWithTwoFullLines(t *testing.T) {
+	myBoard := Init()
+
+	for idx := 6; idx > 0; idx-- {
+		myBoard.MakeMove(2, Red)
+		myBoard.MakeMove(4, Yellow)
+	}
+
+	moves := myBoard.ValidMoves()
+
+	assert.Equal(t, []int{0, 1, 3, 5, 6}, moves)
+}
