aeb08a7500a9d15010a5e581848ca1a46c94c301
Ewout Quax <ewout.quax@bettyblocks.com>
2023-08-09 17:59:57 +0200

    Can detect winning situation, per chip

--- internals/board/ending.go
+++ internals/board/ending.go
@@ -9,3 +9,70 @@ func (b *Board) IsFull() bool {
 
 	return true
 }
+
+func (b *Board) IsWinner(c Chip) bool {
+	return isWinnerHorizontal(b, c) ||
+		isWinnerVertical(b, c) ||
+		isWinnerForwardDiagonal(b, c) ||
+		isWinnerBackwardDiagonal(b, c)
+}
+
+func isWinnerHorizontal(b *Board, c Chip) bool {
+	for idxRow := 5; idxRow >= 0; idxRow-- {
+		for idxLine := 0; idxLine <= 3; idxLine++ {
+			if b.Line[idxLine+0][idxRow] == c &&
+				b.Line[idxLine+1][idxRow] == c &&
+				b.Line[idxLine+2][idxRow] == c &&
+				b.Line[idxLine+3][idxRow] == c {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func isWinnerVertical(b *Board, c Chip) bool {
+	for idxRow := 5; idxRow >= 3; idxRow-- {
+		for idxLine := 0; idxLine <= 6; idxLine++ {
+			if b.Line[idxLine][idxRow-0] == c &&
+				b.Line[idxLine][idxRow-1] == c &&
+				b.Line[idxLine][idxRow-2] == c &&
+				b.Line[idxLine][idxRow-3] == c {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func isWinnerForwardDiagonal(b *Board, c Chip) bool {
+	for idxRow := 0; idxRow <= 2; idxRow++ {
+		for idxLine := 0; idxLine <= 3; idxLine++ {
+			if b.Line[idxLine+0][idxRow+0] == c &&
+				b.Line[idxLine+1][idxRow+1] == c &&
+				b.Line[idxLine+2][idxRow+2] == c &&
+				b.Line[idxLine+3][idxRow+3] == c {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func isWinnerBackwardDiagonal(b *Board, c Chip) bool {
+	for idxRow := 5; idxRow >= 3; idxRow-- {
+		for idxLine := 0; idxLine <= 3; idxLine++ {
+			if b.Line[idxLine+0][idxRow-0] == c &&
+				b.Line[idxLine+1][idxRow-1] == c &&
+				b.Line[idxLine+2][idxRow-2] == c &&
+				b.Line[idxLine+3][idxRow-3] == c {
+				return true
+			}
+		}
+	}
+
+	return false
+}
--- internals/board/ending_test.go
+++ internals/board/ending_test.go
@@ -18,3 +18,71 @@ func TestIsFullForFullBoard(t *testing.T) {
 
 	assert.True(t, board.IsFull())
 }
+
+func TestIsNotWinner(t *testing.T) {
+	board := Init()
+
+	assert.False(t, board.IsWinner(Red))
+}
+
+func TestIsWinnerHorizontal(t *testing.T) {
+	board := Init()
+
+	board.MakeMove(1, Red)
+	board.MakeMove(2, Red)
+	board.MakeMove(3, Red)
+	board.MakeMove(4, Red)
+
+	assert.True(t, board.IsWinner(Red))
+	assert.False(t, board.IsWinner(Yellow))
+}
+
+func TestIsWinnerVertical(t *testing.T) {
+	board := Init()
+
+	board.MakeMove(2, Red)
+	board.MakeMove(2, Red)
+	board.MakeMove(2, Red)
+	board.MakeMove(2, Red)
+
+	assert.True(t, board.IsWinner(Red))
+	assert.False(t, board.IsWinner(Yellow))
+}
+
+func TestIsWinnerForwardDiagonal(t *testing.T) {
+	board := Init()
+
+	board.MakeMove(1, Yellow)
+	board.MakeMove(2, Yellow)
+	board.MakeMove(2, Yellow)
+	board.MakeMove(3, Yellow)
+	board.MakeMove(3, Yellow)
+	board.MakeMove(3, Yellow)
+
+	board.MakeMove(0, Red)
+	board.MakeMove(1, Red)
+	board.MakeMove(2, Red)
+	board.MakeMove(3, Red)
+
+	assert.True(t, board.IsWinner(Red))
+	assert.False(t, board.IsWinner(Yellow))
+}
+
+func TestIsWinnerBackwardDiagonal(t *testing.T) {
+	board := Init()
+
+	board.MakeMove(1, Yellow)
+	board.MakeMove(1, Yellow)
+	board.MakeMove(1, Yellow)
+	board.MakeMove(2, Yellow)
+	board.MakeMove(2, Yellow)
+	board.MakeMove(3, Yellow)
+
+	board.MakeMove(1, Red)
+	board.MakeMove(2, Red)
+	board.MakeMove(3, Red)
+	board.MakeMove(4, Red)
+
+	assert.True(t, board.IsWinner(Red))
+	assert.False(t, board.IsWinner(Yellow))
+}
